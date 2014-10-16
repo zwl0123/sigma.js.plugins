49f7b190255d728920ae5cd011048f1d2199ebd3
Eric Kort <eric.kort@gmail.com>
2014-10-16 16:07:54 -0400

    initial commit

---
+++ sigma.plugins.renderers.borderNodes.js
@@ -0,0 +1,166 @@
+;(function() {
+  'use strict';
+
+  sigma.utils.pkg('sigma.canvas.nodes');
+
+  /**
+   * The default node renderer. It renders the node as a simple disc.
+   *
+   * @param  {object}                   node     The node object.
+   * @param  {CanvasRenderingContext2D} context  The canvas context.
+   * @param  {configurable}             settings The settings function.
+   */
+  sigma.canvas.nodes.bordered = function(node, context, settings) {
+    var prefix = settings('prefix') || '';
+
+    context.fillStyle = node.color || settings('defaultNodeColor');
+    context.lineWidth = 2;
+    context.strokeStyle = darkerColor(node.color, 0.2);
+    context.beginPath();
+    context.shadowBlur = 12;
+    context.shadowColor = node.color;
+     context.arc(
+      node[prefix + 'x'],
+      node[prefix + 'y'],
+      node[prefix + 'size'] * 1,
+      0,
+      Math.PI * 2,
+      true
+    );
+
+    context.closePath();
+    context.fill();
+    context.stroke();
+  };
+
+  // selected node
+  sigma.canvas.nodes.selected = function(node, context, settings) {
+    var prefix = settings('prefix') || '';
+    context.fillStyle = node.color || settings('defaultNodeColor');
+    context.lineWidth = 6;
+    context.strokeStyle = "#fff";
+    context.beginPath();
+    context.shadowBlur = 8;
+    context.shadowColor = "#666";
+     context.arc(
+      node[prefix + 'x'],
+      node[prefix + 'y'],
+      node[prefix + 'size'] * 2,
+      0,
+      Math.PI * 2,
+      true
+    );
+
+    context.closePath();
+    context.fill();
+    context.stroke();
+    
+    context.lineWidth = 2;
+    context.strokeStyle = "#444";
+    context.beginPath();
+     context.arc(
+      node[prefix + 'x'],
+      node[prefix + 'y'],
+      node[prefix + 'size'] * 2,
+      0,
+      Math.PI * 2,
+      true
+    );
+    context.closePath();
+    context.fill();
+    context.stroke();
+  };
+
+})();
+
+
+/**
+ * Adjust value ("darkness") of RGB color to darker or lighter color
+ * 
+ * Code obtained from StackOverflow.com: http://goo.gl/uGGqvI 
+ * Thanks to user @eyelidlessness!
+ *
+ * @param  {color}                    the original color in hex format
+ * @param  {ratio}                    number between and 0 & 1: how much to adjust?
+ * @param  {darker}                   do you want new color darker or lighter?
+ */
+
+var changeColor = function(color, ratio, darker) {
+    // Trim trailing/leading whitespace
+    color = color.replace(/^\s*|\s*$/, '');
+
+    // Expand three-digit hex
+    color = color.replace(
+        /^#?([a-f0-9])([a-f0-9])([a-f0-9])$/i,
+        '#$1$1$2$2$3$3'
+    );
+
+    // Calculate ratio
+    var difference = Math.round(ratio * 256) * (darker ? -1 : 1),
+        // Determine if input is RGB(A)
+        rgb = color.match(new RegExp('^rgba?\\(\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '\\s*,\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '\\s*,\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '(?:\\s*,\\s*' +
+            '(0|1|0?\\.\\d+))?' +
+            '\\s*\\)$'
+        , 'i')),
+        alpha = !!rgb && rgb[4] != null ? rgb[4] : null,
+
+        // Convert hex to decimal
+        decimal = !!rgb? [rgb[1], rgb[2], rgb[3]] : color.replace(
+            /^#?([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])/i,
+            function() {
+                return parseInt(arguments[1], 16) + ',' +
+                    parseInt(arguments[2], 16) + ',' +
+                    parseInt(arguments[3], 16);
+            }
+        ).split(/,/),
+        returnValue;
+
+    // Return RGB(A)
+    return !!rgb ?
+        'rgb' + (alpha !== null ? 'a' : '') + '(' +
+            Math[darker ? 'max' : 'min'](
+                parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+            ) + ', ' +
+            Math[darker ? 'max' : 'min'](
+                parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+            ) + ', ' +
+            Math[darker ? 'max' : 'min'](
+                parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+            ) +
+            (alpha !== null ? ', ' + alpha : '') +
+            ')' :
+        // Return hex
+        [
+            '#',
+            pad(Math[darker ? 'max' : 'min'](
+                parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+            ).toString(16), 2),
+            pad(Math[darker ? 'max' : 'min'](
+                parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+            ).toString(16), 2),
+            pad(Math[darker ? 'max' : 'min'](
+                parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+            ).toString(16), 2)
+        ].join('');
+};
+var lighterColor = function(color, ratio) {
+    return changeColor(color, ratio, false);
+};
+var darkerColor = function(color, ratio) {
+    return changeColor(color, ratio, true);
+};
+
+var pad = function(num, totalChars) {
+    var pad = '0';
+    num = num + '';
+    while (num.length < totalChars) {
+        num = pad + num;
+    }
+    return num;
+};
